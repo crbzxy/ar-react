57e7e72c682f96c56a7dbe8aa2390107e97e3368
Carlos Boyzo <carlos.boor@gmail.com>
2021-07-24 09:04:01 -0500

    modelo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
                     <a-entity
           position="0 0 -1.5"
           scale="0.05 0.05 0.05"
-          gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+          gltf-model="./models/Numero1.glbt"
         ></a-entity>
                     </a-anchor>
                     <a-camera-static preset="hiro" />
