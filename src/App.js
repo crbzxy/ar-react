0d14f1c67a3a892714aee42e44cdd55210efdeb6
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-07-24 18:22:06 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a-scene  arjs='sourceType: webcam; debugUIEnabled: false'>
+                    <a-anchor hit-testing-enabled="true">
+                    <a-entity
+          position="0 0 -1.5"
+          scale="0.05 0.05 0.05"
+          gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+        ></a-entity>
+                    </a-anchor>
+                    <a-camera-static preset="hiro" />
+                </a-scene>
     </div>
   );
 }
